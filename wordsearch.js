a36315beef2d64325eefe4834d69390ad1d3759d
Brandon Copeland <copelandbrandon2@gmail.com>
2021-05-06 01:05:18 +0000

    removed some blank space

--- wordsearch.js
+++ wordsearch.js
@@ -15,19 +15,9 @@ const transpose = function (matrix) {
 const wordSearch = (letters, word) => { 
   const horizontalJoin = letters.map(ls => ls.join(''))
   const verticalJoin = transpose(letters).map(ls => ls.join(''));
-//   const backwardsArray=letter.reverse
-  //const backwardsLetters = letters.map(ls => ls.reverse().join(''));
-  //const vertBackwardsLetters = transpose(letters).map(ls => ls.reverse().join(''));
+
   const backwardsWord = word.split('').reverse().join('');
-  console.log(backwardsWord);
-  // console.log(transpose(letters));
-  // console.log(vertBackwardsLetters);
-//   for (l of backwardsLetters) {
-//     if (l.includes(word)) return true;
-// }
-//   for (l of vertBackwardsLetters) {
-//     if (l.includes(word)) return true;
-// }
+
     for (l of horizontalJoin) {
         if (l.includes(word)) return true;
     }
@@ -46,18 +36,5 @@ const wordSearch = (letters, word) => {
     return false;
 }
 
-
-const result = wordSearch([
-  ['S', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-  ['E', 'K', 'L', 'N', 'F', 'E', 'L', 'D'],
-  ['I', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-  ['N', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-  ['F', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-  ['E', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-  ['L', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-  ['D', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-  ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-], 'LSIBLGLDL')
-console.log(result);
 
 module.exports = wordSearch
